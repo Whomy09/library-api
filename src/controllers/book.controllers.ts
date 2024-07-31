a702139819df22ca9cc4a8b5326aa5bca1200c83
Jesus Berdugo <berdugopertuz@gmail.com>
2024-07-30 19:01:46 -0500

    feat: get book by id

--- src/controllers/book.controllers.ts
+++ src/controllers/book.controllers.ts
@@ -1,6 +1,7 @@
 import { Book } from "@/models/book.model";
 import { Request, Response } from "express";
 import { IBook } from "@/schemas/bookSchema";
+import mongoose from "mongoose";
 
 export const create = async (req: Request<{}, {}, IBook>, res: Response) => {
   try {
@@ -14,6 +15,32 @@ export const create = async (req: Request<{}, {}, IBook>, res: Response) => {
       res.status(400).json({ message: error.message });
     } else {
       res.status(400).json({ message: error });
+    }
+  }
+};
+
+export const getById = async (
+  req: Request<{ id: string }, {}, {}>,
+  res: Response
+) => {
+  try {
+    const id = req.params.id;
+
+    if (!id) {
+      throw new Error("ID is required");
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new Error("Invalid ID format");
+    }
+
+    const book = await new Book().getById(id);
+    res.status(200).json(book);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({ message: error.message });
+    } else {
+      res.status(400).json({ error: error });
     }
   }
 };
--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -12,4 +12,23 @@ export class Book {
   async create(book: IBook) {
     await new this.db(book).save();
   }
+
+  async getById(id: string) {
+    /**
+     * Note: The lean function returns 
+     * me a flat js object if metadata
+     */
+    const book = await this.db.findById(id).lean();
+
+    if (!book) {
+      throw new Error("Book not found");
+    }
+ 
+    const { _id, ..._book } = book
+
+    return {
+      id: _id,
+      ..._book
+    }
+  }
 }
